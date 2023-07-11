581e433ab4f169f688f52b6b3683389c8993dd8b
Serhii Hulenko <sergei.gulenko@gmail.com>
2023-07-11 23:40:29 +0200

    Add theme (#14)
    - add AppColors
    - add AppTheme
    - add AppTypography
    - update IntroScreen
    - update SplashScreen
    - migration from material to material3

--- shared/src/commonMain/kotlin/presentation/screen/intro/IntroScreen.kt
+++ shared/src/commonMain/kotlin/presentation/screen/intro/IntroScreen.kt
@@ -2,13 +2,14 @@ package presentation.screen.intro
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import cafe.adriel.voyager.core.screen.Screen
+import presentation.ui.AppTheme
 
 internal object IntroScreen : Screen {
 
@@ -17,12 +18,23 @@ internal object IntroScreen : Screen {
     override fun Content() {
         Box(
             modifier = Modifier.fillMaxSize()
-                .background(Color.Blue),
+                .background(AppTheme.colors.additional.background),
         ) {
-            Text(
-                text = "Intro",
+            Column(
                 modifier = Modifier.align(Alignment.Center),
-            )
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "Intro Screen",
+                    style = AppTheme.typography.h4Bold,
+                    color = AppTheme.colors.grayscale.gray100,
+                )
+                Text(
+                    text = "Sample",
+                    style = AppTheme.typography.bodyMediumMedium,
+                    color = AppTheme.colors.grayscale.gray40,
+                )
+            }
         }
     }
 }
---
+++ shared/src/commonMain/kotlin/presentation/ui/AppColors.kt
@@ -0,0 +1,108 @@
+package presentation.ui
+
+import androidx.compose.ui.graphics.Color
+
+data class AppColors(
+    val main: MainColors,
+    val alerts: AlertColors,
+    val additional: AdditionalColors,
+    val grayscale: GrayscaleColors,
+    val isLight: Boolean,
+) {
+
+    data class MainColors(
+        val primary: Color,
+        val secondary: Color,
+    )
+
+    data class AlertColors(
+        val error: Color,
+        val warning: Color,
+        val success: Color,
+    )
+
+    data class AdditionalColors(
+        val white: Color,
+        val background: Color,
+        val background2: Color,
+        val backgroundBlue: Color,
+        val line: Color,
+    )
+
+    data class GrayscaleColors(
+        val gray10: Color,
+        val gray20: Color,
+        val gray30: Color,
+        val gray40: Color,
+        val gray50: Color,
+        val gray60: Color,
+        val gray70: Color,
+        val gray80: Color,
+        val gray90: Color,
+        val gray100: Color,
+    )
+}
+
+internal val lightColors = AppColors(
+    main = AppColors.MainColors(
+        primary = Color(0xFF4864E1),
+        secondary = Color(0xFFF984A0),
+    ),
+    alerts = AppColors.AlertColors(
+        error = Color(0xFFE88888),
+        warning = Color(0xFFFACC15),
+        success = Color(0xFF4EBE88),
+    ),
+    additional = AppColors.AdditionalColors(
+        white = Color(0xFFFFFFFF),
+        background = Color(0xFFFAFAFA),
+        background2 = Color(0xFFFAFAFA),
+        backgroundBlue = Color(0xFFD8E5FE),
+        line = Color(0xFFDDDDDD),
+    ),
+    grayscale = AppColors.GrayscaleColors(
+        gray10 = Color(0xFFF2F2F7),
+        gray20 = Color(0xFFD1D1D6),
+        gray30 = Color(0xFFFFCD1A),
+        gray40 = Color(0xFFAEAEB2),
+        gray50 = Color(0xFFFFCD1A),
+        gray60 = Color(0xFF09080D),
+        gray70 = Color(0xFF08070C),
+        gray80 = Color(0xFF07060A),
+        gray90 = Color(0xFF060508),
+        gray100 = Color(0xFF1C1C1E),
+    ),
+    isLight = true,
+)
+
+internal val darkColors = AppColors(
+    main = AppColors.MainColors(
+        primary = Color(0xFF4864E1),
+        secondary = Color(0xFFF984A0),
+    ),
+    alerts = AppColors.AlertColors(
+        error = Color(0xFFE88888),
+        warning = Color(0xFFFACC15),
+        success = Color(0xFF4EBE88),
+    ),
+    additional = AppColors.AdditionalColors(
+        white = Color(0xFFFFFFFF),
+        background = Color(0xFF1C1C1E),
+        background2 = Color(0xFF002380),
+        backgroundBlue = Color(0xFFDDDDDD),
+        line = Color(0xFFDDDDDD),
+    ),
+    grayscale = AppColors.GrayscaleColors(
+        gray10 = Color(0xFFF2F2F7),
+        gray20 = Color(0xFFD1D1D6),
+        gray30 = Color(0xFFFFCD1A),
+        gray40 = Color(0xFFAEAEB2),
+        gray50 = Color(0xFFFFCD1A),
+        gray60 = Color(0xFF09080D),
+        gray70 = Color(0xFF08070C),
+        gray80 = Color(0xFF07060A),
+        gray90 = Color(0xFF060508),
+        gray100 = Color(0xFFFFFFFF),
+    ),
+    isLight = false,
+)
