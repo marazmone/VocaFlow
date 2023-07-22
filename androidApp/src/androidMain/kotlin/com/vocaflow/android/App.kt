b9c1a499814ee9441d52a350afb7e4328aeb8244
Serhii Hulenko <sergei.gulenko@gmail.com>
2023-07-22 18:43:08 +0200

    Update DI

--- androidApp/src/androidMain/kotlin/com/vocaflow/android/App.kt
+++ androidApp/src/androidMain/kotlin/com/vocaflow/android/App.kt
@@ -7,14 +7,21 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.logger.Level.ERROR
 import org.koin.core.logger.Level.NONE
+import presentation.text.Strings
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initStrings()
         initKoin {
             androidLogger(level = if (DEBUG) ERROR else NONE)
             androidContext(androidContext = this@App)
         }
+    }
+
+    // This is workaround resolving the issue with the context not being available in the shared module
+    private fun initStrings() {
+        Strings.context = this
     }
 }
